3a0e213c5a1d15f03dd10f12116c1e13b205215e
Helberte <helberteads@gmail.com>
2024-06-11 12:55:18 -0400

    iniciando configuracao do servidor

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,2 +1,35 @@
-// import express from "express";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+export class Server{
+
+  readonly express: express.Express;
+
+  constructor() {
+    this.express = express();
+  }
+
+  private middlewares() {
+    this.express.use(cookieParser());
+    this.express.use(express.json());
+    this.express.use(cors({
+        credentials: true,
+        origin: String(process.env.CORS_ORIGIN)
+    }));
+  }
+
+  environment() {
+
+  }
+
+  async start() {
+    
+  }
+
+  public stop() {
+    
+  }
+}
+
+export default new Server();
