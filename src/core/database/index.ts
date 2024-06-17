ffe543361f6be5448a757d3ee82320818f93fcc2
Helberte Costa Arruda <helberteads@gmail.com>
2024-06-17 18:01:30 -0400

    configuracao dos models e da instancia do servidor

--- src/core/database/index.ts
+++ src/core/database/index.ts
@@ -6,11 +6,31 @@ class Database {
   private conexao: Sequelize;
 
   async config() {
-    this.conexao = new Sequelize(process.env.DB_MYSQL_DATABASE, process.env.DB_MYSQL_USERNAME, process.env.DB_MYSQL_PASSWORD, {
-      host: process.env.DB_MYSQL_HOST,
-      dialect: process.env.DB_MYSQL_DIALECT as Dialect,
-      models: [__dirname.substring(0, __dirname.indexOf("dist")) + "dist\\src\\models\\mundo_verde"]
-    });
+    this.conexao = new Sequelize(
+      process.env.DB_MYSQL_DATABASE,
+      process.env.DB_MYSQL_USERNAME,
+      process.env.DB_MYSQL_PASSWORD, {
+        host: process.env.DB_MYSQL_HOST,
+        dialect: process.env.DB_MYSQL_DIALECT as Dialect,
+        models: [__dirname.substring(0, __dirname.indexOf("dist")) + "dist\\src\\models\\mundo_verde"],
+        define: {
+          defaultScope: {
+            attributes: {
+              exclude: ["deletedBy", "updatedBy", "createdBy", "deletedAt", "updatedAt", "createdAt"]
+            }
+          }
+        },
+        pool: {
+          max: 100,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        },
+        dialectOptions: {
+          connectTimeout: 10000
+        }
+      }
+    );
 
     try {
       await this.conexao.authenticate();
--- src/models/mundo_verde/estado.ts
+++ src/models/mundo_verde/estado.ts
@@ -1,8 +1,8 @@
 import { Moment } from "moment";
-import { Table, Column, Model, AutoIncrement, PrimaryKey, AllowNull } from "sequelize-typescript";
+import { Table, Column, Model, AutoIncrement, PrimaryKey, AllowNull, DataType, CreatedAt, UpdatedAt, DeletedAt } from "sequelize-typescript";
 
 @Table({ tableName: "estado" })
-export default class Estado extends Model {
+export default class Estado extends Model<Estado> {
 
   @AutoIncrement
   @PrimaryKey
@@ -21,22 +21,24 @@ export default class Estado extends Model {
   @Column
   ibge_id: number
 
-  @AllowNull(false)
-  @Column("created_at")
+  @CreatedAt
+  @Column({ field: "created_at", type: DataType.DATE})
   createdAt: Moment;
 
-  @Column("updated_at")
+  @UpdatedAt
+  @Column({ field: "updated_at", type: DataType.DATE})
   updatedAt: Moment;
 
-  @Column("deleted_at")
+  @DeletedAt
+  @Column({ field: "deleted_at", type: DataType.DATE})
   deletedAt: Moment;
 
-  @Column("created_by")
+  @Column({ field: "created_by", type: DataType.STRING})
   createdBy: Moment;
 
-  @Column("updated_by")
+  @Column({ field: "updated_by", type: DataType.STRING})
   updatedBy: Moment;
 
-  @Column("deleted_by")
+  @Column({ field: "deleted_by", type: DataType.STRING})
   deletedBy: Moment;
 }
